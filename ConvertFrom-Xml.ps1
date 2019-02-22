2e90c043767133d79225ff14d0f89f4b40a1e4d5
Pier-Luc Duchaine <pier-luc.duchaine@groupecanam.com>
2019-02-21 22:04:22 -0500

    Add ConvertFrom-Xml script

---
+++ ConvertFrom-Xml.ps1
@@ -0,0 +1,54 @@
+
+<#PSScriptInfo
+.VERSION 1.0
+.GUID 0c3a31f4-028b-498e-afa0-3ed0d22cac6a
+.AUTHOR Pier-Luc.Duchaine@groupecanam.com
+.COMPANYNAME 
+.COPYRIGHT 
+.TAGS 
+.LICENSEURI 
+.PROJECTURI 
+.ICONURI 
+.EXTERNALMODULEDEPENDENCIES 
+.REQUIREDSCRIPTS 
+.EXTERNALSCRIPTDEPENDENCIES 
+.RELEASENOTES
+#>
+
+<# 
+.DESCRIPTION 
+ Convert XmlElement to PSObject 
+
+ Usage: 
+     [xml]$xmlDocument = $xmlFile | Get-Content
+     $xmlDocument.DocumentElement | ConvertFrom-Xml
+
+ This also work in conjunction with "ConvertTo-Json" to convert Xml to Json
+#> 
+
+[CmdletBinding()]
+Param(
+    [System.Xml.XmlElement][Parameter(ValueFromPipeline)]$xmlElement
+)
+
+if($xmlElement.InnerText -ne ""){
+    $xmlElement.InnerText
+}
+else {
+    $PSObject = New-Object PSObject
+    
+    foreach($attribute in $xmlElement.Attributes){
+        $PSObject | Add-Member NoteProperty $attribute.Name $attribute.Value
+    }  
+
+    if($xmlElement.ChildNodes.Count -ne 0) {
+        $PSArray = @()
+        
+        foreach($data in $xmlElement.ChildNodes){
+            $PSArray += $data | ConvertFrom-Xml
+        }
+        $PSObject | Add-Member NoteProperty $xmlElement.LocalName $PSArray
+    }
+    
+    $PSObject 
+}
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # PowerShellScripts
+
+Collection of some Scripts I use in different cases
+
+## ConvertFrom-Xml
+
+Simple tool that I use in a Pipeline to convert Xml to Json
+
+Not tested with many cases, but it works really well with the cases I had
+
+```powershell
+[xml]$xmlDocument = $xmlFile | Get-Content
+$json = $xmlDocument.DocumentElement | ConvertFrom-Xml | ConvertTo-Json
+```
